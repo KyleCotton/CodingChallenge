14b0e549772841c68ef55db61a1eda6e3e4b23e3
Kyle Cotton <kylecottonkc@gmail.com>
2018-11-02 10:14:08 +0000

    Added Main Haskell Source File

---
+++ Haq.hs
@@ -0,0 +1,13 @@
+-- Haskell Programming Challenge
+-- Created By: Kyle Cotton, Jasper Kettle, Dylan Lyons, Yannik Nelson
+-- ~~~ Game of Life ~~~
+
+{- NOTES:
+To Compile: ghc Haq.hs
+Toi Run   : ./Haq
+-}
+
+import System.Environment
+main :: IO ()
+main = putStrLn "Game of Life!"
+ 
