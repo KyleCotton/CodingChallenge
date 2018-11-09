d0660e0e431eaa4dce0c48082cdee3dc4eafe09c
Kyle Cotton <kylecottonkc@gmail.com>
2018-11-09 20:30:22 +0000

    Optimimised Code

--- Game.hs
+++ Game.hs
@@ -19,18 +19,22 @@ Dead:  3 Friends   -> Alive
 -}
 
 -- WRITE STRIP FUNCTION TO REMOVE ALL OF THE FALSE FROM THE GRID
---strip g = filter (\(h, l) -> h) g
+
+-- ### Not Working
+strip :: [Grid] -> [Grid]
+strip gs = map ( filter (\(h, l) -> h) ) gs
 
 nIterations :: Int -> Grid -> [Grid]
 nIterations n g = take n $ iterate (nextGen) g
 
-startPeople, startPeople' :: [Person]
-startPeople = [(False,(0,0)),(True,(0,1)),(False,(0,2)),(True,(0,3)),(False,(0,4)), (True, (1,2))]
-
-startPeople' = [(False,(0,0)),(False,(0,1)),(True,(0,2)),(False,(0,3)),(False,(0,4)), (True, (1,2))]
-
-emptyGrid :: Int -> Int -> Grid
-emptyGrid n p = [(False, (x,y)) | x <- [0..(n-1)], y <-[0..(p-1)]]
+startPeople :: Grid
+startPeople = [
+  (False,(0,0)),(True,(0,1)),(False,(0,2)),(True,(0,3)),(False,(0,4))
+  (False,(1,0)),(True,(1,1)),(False,(1,2)),(True,(1,3)),(False,(1,4))
+  (False,(2,0)),(True,(2,1)),(False,(2,2)),(True,(2,3)),(False,(2,4))
+  (False,(3,0)),(True,(3,1)),(False,(3,2)),(True,(3,3)),(False,(3,4))
+  (False,(4,0)),(True,(4,1)),(False,(4,2)),(True,(4,3)),(False,(4,4))
+  ]
 
 nextGen :: Grid -> Grid
 nextGen gs = map (\p@(h, l) -> (isAlive p gs, l)) gs
@@ -39,10 +43,10 @@ isAlive :: Person -> Grid -> Bool
 isAlive (h, (x, y)) gss = case h of
                             True  -> gs == 2 || gs == 3
                             False -> gs == 3
-                          where gs = length [1 | (h', (x', y'))
-                                               <- gss, x' `elem` [x-1,x,x+1]
-                                               , y' `elem` [y-1,y,y+1]
-                                               , h', (x/=x' || y/=y')] 
+                          where
+                            gs = length [1 | (h', (x', y')) <- gss, x' `elem` [x-1,x,x+1]
+                                                                  , y' `elem` [y-1,y,y+1]
+                                                                  , h', (x/=x' || y/=y')]
 
 
 
