5cefc32f7a4e9efd04c760c08474529450dad9e0
yanniknelson <yannik@nelsonfamily.org.uk>
2018-11-16 14:08:30 +0000

    Added more framework for 3d rendering

--- rendering.hs
+++ rendering.hs
@@ -2,17 +2,18 @@ module Rendering where
 
 import Data.List
 import Test.QuickCheck
-import Graphics.UI.GLUT hiding (Matrix, Angle)
+import Graphics.UI.GLUT hiding (Matrix, Angle, project)
 import Data.IORef
 import Control.Concurrent
 import Game
 
 myPoints :: [Point]
-myPoints = [(0.5,0.5), (-0.5, 0.5), (-0.5, -0.5), (0.5, -0.5)]
+myPoints = [(0,0,0)]
 
 type Matrix = [[GLfloat]]
 type Vector = [GLfloat]
-type Point = (GLfloat, GLfloat)
+type Point = (GLfloat, GLfloat, GLfloat)
+type Point2D = (GLfloat, GLfloat)
 type Angle = GLfloat
 
 gridWidth :: Float
@@ -21,12 +22,15 @@ gridWidth = 100
 gridHight :: Float
 gridHight = 100
 
-makeSquare :: GLfloat -> Point -> [Point]
-makeSquare size center = moveSquare byOrigin center
+makeCube :: GLfloat -> Point -> [Point]
+makeCube size center = moveCube byOrigin center
   where
-    moveSquare xs (x, y)= [((x + x'), (y + y'))| (x', y') <- xs]
-    byOrigin = [(radius', radius'), (-radius', radius'), (-radius', -radius'), (radius', -radius')]
+    byOrigin = [(radius', radius', radius'), (-radius', radius', radius'), (-radius', -radius', radius'), (radius', -radius', radius'),
+                (radius', radius', -radius'), (-radius', radius', -radius'), (-radius', -radius', -radius'), (radius', -radius', -radius')]
     radius' = (size/ 2)
+
+moveCube :: [Point] -> Point -> [Point]
+moveCube xs (x, y, z)= [((x + x'), (y + y'), (z+z'))| (x', y', z') <- xs]
 
 --Takes two matrices and multiplies them
 multiplyMat :: Matrix -> Matrix -> Matrix
@@ -40,31 +44,83 @@ multMatVec mat vec = head (transpose (multiplyMat mat (transpose [vec])))
 --points can only have two values so an ordered list is used
 --and the rest of the values in the vector are discarded.
 vecToPoint :: Vector -> Point
-vecToPoint (x:y:ns) = (x,y)
+vecToPoint (x:y:z:ns) = (x,y,z)
 
 --Takes a point and converts it to a 2D vector
 pointToVec :: Point -> Vector
-pointToVec (x,y) = [x,y]
+pointToVec (x,y,z) = [x,y,z]
 
 --Takes in a vecotr and an anple and returns the vector rotated around the origin by the given angle
-rotate2D :: Angle -> Vector -> Vector
-rotate2D t m = multMatVec rotation m
-  where
-    rotation  = [[cos (radians t), (-1) * (sin (radians t))], [sin (radians t), cos (radians t)]]
-    radians t =  t * 2 * pi / 360
+rotateX :: Angle -> Vector -> Vector
+rotateX t m = multMatVec (rotationX t) m
+
+rotationX t = [[1,0,0],
+              [0,cos (radians t), -sin (radians t)],
+              [0,sin (radians t), cos (radians t)]]
+
+cRotationX t = [[1,0,0],
+              [0,cos (radians t), sin (radians t)],
+              [0,-sin (radians t), cos (radians t)]]
+
+rotateY :: Angle -> Vector -> Vector
+rotateY t m = multMatVec (rotationY t) m
+
+rotationY t = [[cos (radians t), 0, -sin (radians t)],
+              [0,1,0],
+              [sin (radians t), 0, cos (radians t)]]
+
+rotateZ :: Angle -> Vector -> Vector
+rotateZ t m = multMatVec (rotationZ t) m
+
+rotationZ t = [[cos (radians t), -sin (radians t), 0],
+              [sin (radians t), cos (radians t),0],
+              [0,0,1]]
+
+cRotationZ t = [[cos (radians t), sin (radians t), 0],
+              [-sin (radians t), cos (radians t),0],
+              [0,0,1]]
+
+radians :: Floating a => a -> a
+radians t =  t * 2 * pi / 360
 
 --Takes an angle and list of points and rotates each of the poins around the origin
 rotatePoints :: Angle -> [Point] -> [Point]
-rotatePoints theta pts = [ vecToPoint . (rotate2D theta) $ pointToVec pt| pt <- pts]
+rotatePoints theta pts = [ vecToPoint . (rotateZ theta) $ pointToVec pt| pt <- pts]
 
-mapPoints :: [Point] -> [Point]
-mapPoints lst = [(((x-xOffset)/xOffset)+(1/gridWidth), -(((y-yOffset)/yOffset) + (1/gridWidth))) | (y,x)<-lst]
+--mapPoints :: [Point] -> [Point]
+--mapPoints lst = [(((x-xOffset)/xOffset)+(1/gridWidth), -(((y-yOffset)/yOffset) + (1/gridWidth))) | (y,x)<-lst]
+ -- where
+   -- xOffset = (gridWidth/2)
+   -- yOffset = (gridHight/2)
+
+makeCubes :: [Point] -> [Point]
+makeCubes lst = concat [makeCube (2/gridWidth) point | point<-lst]
+
+subVectors :: Vector -> Vector -> Vector
+subVectors v1 v2 = [(v1 !! a) - (v2 !! a) | a <- [0..(length v1 - 1)]]
+
+projects :: (Angle, Angle, Angle) -> [Point] -> [Point2D]
+projects = undefined
+
+--project :: (Angle, Angle, Angle) -> [Point] -> [Point] -> [Point] -> [Point2D]
+project (ax, ay, az) pointPos camPos =  multMatVec  (multiplyMat (multiplyMat (cRotationX ax) (rotationY ay)) (cRotationZ az)) (subVectors pointPos camPos)
+
+project' (ax, ay, az) pointPos camPos = proj $ vecToPoint (subVectors pointPos camPos)
   where
-    xOffset = (gridWidth/2)
-    yOffset = (gridHight/2)
+    proj points  = [dx points , dy points, dz points]
+    cx = cos(radians ax)
+    cy = cos(radians ay)
+    cz = cos(radians az)
+    sx = sin(radians ax)
+    sy = sin(radians ay)
+    sz = sin(radians az)
+    dx (x,y,z) = (cy*(sz*y + cz*x) - sy*z)
+    dy (x,y,z) = (sx*(cy*z + sy*(sz*y + cz*x)) + cx*(cz*y - sz*x))
+    dz (x,y,z) = (cx*(cy*z + sy*(sz*y + cz*x)) - sx*(cz*y - sz*x))
 
-makeSquares :: [Point] -> [Point]
-makeSquares lst = concat [makeSquare (2/gridWidth) point | point<-lst]
+prop_projects :: (Angle, Angle, Angle) -> Vector -> Vector -> Property
+prop_projects angles pointP camP = length pointP == 3 && length camP == 3 ==> (project angles pointP camP) == (project' angles pointP camP)
+
 
 main :: IO ()
 main = do
@@ -77,9 +133,9 @@ main = do
   enterGameMode
   reshapeCallback $= Just reshape
   --creates a mutatable variable for the angle of rotation
-  angle <- newIORef 0.0
+  cameraAngle <- newIORef (0, 0, 0)
   --displays points
-  displayCallback $= (display angle)
+  displayCallback $= (display cameraAngle)
   --makes changes
   --idleCallback $= Just (idle angle)
   mainLoop
@@ -90,20 +146,20 @@ reshape size = do
   postRedisplay Nothing
 
 --displays the points as a loop
-display :: IORef GLfloat -> DisplayCallback
-display angle = do
+display :: IORef (Angle, Angle, Angle) -> DisplayCallback
+display cameraAngle = do
   --helper function that creates a color
   let color3f r g b = color $ Color3 r g (b :: GLfloat)
   --clears the color buffer
   clear [ ColorBuffer ]
   --gets the value of the mutatable variable and stores it as angle'
-  angle' <- readIORef angle
+  angle' <- readIORef cameraAngle
   --renders groups of four vertexs as squares
   renderPrimitive Quads $ do
     --sets the color to red
     color3f 1 0 0
     --takes a list of points and converts them to vertexs
-    mapM_ (\(x, y) -> vertex $ Vertex2 x y) (makeSquares . mapPoints $ gridToLivingPoints startPeople)
+    mapM_ (\(x, y) -> vertex $ Vertex2 x y) ((projects angle') $ makeCubes myPoints)
   flush
   --limits the frame rate to 60 fps
   threadDelay (1000000 `div` 60)
