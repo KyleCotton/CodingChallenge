e40fce3722bb5f63cc5f0bf10a279253ab381b6f
Kyle Cotton <kylecottonkc@gmail.com>
2018-11-19 19:05:29 +0000

    Implememnted 3D Coordinate System

--- Game.hs
+++ Game.hs
@@ -17,31 +17,31 @@ module Game (Grid, theOtherGrid, nIterations, gridToLivingPoints, gridToDeadPoin
 import Data.List
 
 type Health   = Bool               -- Each Block has an accociated health
-type Location = (Float , Float)    -- Each Block has an cartesian coordinate
+type Location = (Float , Float, Float)    -- Each Block has an cartesian coordinate
 type Person   = (Health, Location) -- Each 'Person' will have a health and location
 type Grid     = [Person]           -- The grid is represented as a list of all the people
 
 theGrid :: Grid
-theGrid = [( ((fromInteger x, fromInteger y) `elem` aliveStates)            -- Define the starting Grid for the game
-           , (fromInteger x, fromInteger y) ) | x <- [0..99], y <- [0..99]] --     The grid will be all false except
+theGrid = [( ((fromInteger x, fromInteger y, fromInteger z) `elem` aliveStates)            -- Define the starting Grid for the game
+           , (fromInteger x, fromInteger y, fromInteger z) ) | x <- [0..99], y <- [0..99], z <- [0..99]] --     The grid will be all false except
                                                                             --     the ones in aliveStates
 theOtherGrid :: Grid
 theOtherGrid = [ (True,l) | l <- aliveStates ]
 
 tOG :: Grid
-tOG = [ (cond (x,y), (fromInteger x, fromInteger y)) | x <- [0..60], y <- [0..60], cond (x,y) ]
+tOG = [ (cond (x,y,z), (fromInteger x, fromInteger y, fromInteger z)) | x <- [0..60], y <- [0..60], z <- [0..60], cond (x,y,z) ]
         where
-          cond (a,b) = ((mod a 7 == 0) && (mod b 7 == 0)) || ((mod a 11 == 0) && (mod b 11 == 0))
+          cond (a,b,c) = ((mod a 7 == 0) && (mod b 7 == 0) && (mod c 7 ==0) || ((mod a 11 == 0) && (mod b 11 == 0) && (mod c 11 == 0)))
 
 aliveStates :: [Location]  -- A list of locations of the alive states
-aliveStates = [(50,50)
-              ,(51,50)
-              ,(49,50)
-              ,(40,40)
-              ,(40,41)
-              ,(40,39)
-              ,(39,41)
-              ,(38,40)]
+aliveStates = [(50,50,50)
+              ,(51,50,50)
+              ,(49,50,50)
+              ,(40,40,50)
+              ,(40,41,50)
+              ,(40,39,50)
+              ,(39,41,50)
+              ,(38,40,50)]
 
 
 nIterations :: Int -> Grid -> [Grid]             -- This returns a list of grids
@@ -64,13 +64,15 @@ getPoints :: Grid -> [Location]
 getPoints = map (\(p,l) -> l)
 
 zombieLocal :: Location -> [Location]
-zombieLocal (x,y) = [ (a,b) | a <- [x-1..x+1], b <- [y-1..y+1], not ((a==x)&&(b==y)) ]
+--zombieLocal (x,y) = [ (a,b) | a <- [x-1..x+1], b <- [y-1..y+1], not ((a==x)&&(b==y)) ]
+zombieLocal (x,y,z) = [ (a,b,c) | a <- [x-1..x+1], b <- [y-1..y+1], c <- [z-1..z+1], a/=x, b/=y, c/=z ]
 
 isAlive :: Person -> Grid -> Bool                                                       -- Function that generates the next grid from the previous
-isAlive (h, (x, y)) gss = let gs = length [1 |  (h', (x', y'))                    --    This fucntion takes in a person and the current grid
+isAlive (h, (x, y, z)) gss = let gs = length [1 |  (h', (x', y', z'))                    --    This fucntion takes in a person and the current grid
                                                    <- gss, x' `elem` [x-1,x,x+1]  --    if the block is alive and 3 or 2 of its neigbours
                                                          , y' `elem` [y-1,y,y+1]  --    are also alive the the block will stay alive.
-                                                         , h',   (x/=x' || y/=y')]  --    if the block is dead and is surrounded by 3 alive
+                                                         , z' `elem` [z-1,z,z+1]
+                                                         , h',   (x/=x' || y/=y' || z/=z')]  --    if the block is dead and is surrounded by 3 alive
                                 in                                                      --    neigbours then it will become alive.
                                   case h of
                                     True  -> (gs == 2 || gs == 3)
